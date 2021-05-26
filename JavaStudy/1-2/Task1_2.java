cd06e012532143764d1dd4cdad461c1391d656e8
Takahashi-Kei <84252685+Takahashi-Kei@users.noreply.github.com>
2021-05-27 00:28:41 +0900

    Update Task1_2.java

--- JavaStudy/1-2/Task1_2.java
+++ JavaStudy/1-2/Task1_2.java
@@ -37,7 +37,7 @@ public class Task1_2 {
         int[] intArray = { 10, 20, 30, 40, 50 };
 
         // ⑥下記の処理について、何をしているのかコメントを記入してください。
-        // [配列intArrayの2番目と5番目の要素を出力]
+        // [配列intArrayの2番目と5番目の要素の合計を出力]
         System.out.println(intArray[1] + intArray[4]);
 
     }
